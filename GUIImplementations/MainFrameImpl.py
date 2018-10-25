d0914c7d2c3668ea8e4c741e3e1dabf15c925c8e
craig <craig.rodger@gmail.com>
2018-10-25 19:39:18 +1100

    Basic algorithm application done

--- GUIImplementations/MainFrameImpl.py
+++ GUIImplementations/MainFrameImpl.py
@@ -8,13 +8,15 @@ import wx
 import cv2
 from GUILayouts.OpenCVExplorerGUI import MainFrameDefn
 from wx.lib.dialogs import openFileDialog
-import Utilities.OpenCVOperations
 from Utilities.OpenCVOperations import allOperations
 
 class MainFrameImpl(MainFrameDefn):
     
     bmp = None
     baseImage = None
+    blur = False
+    canny = False
+    plotContour = None
     
     def __init__(self, parent:MainFrameDefn):
         MainFrameDefn.__init__(self, parent)
@@ -27,7 +29,7 @@ class MainFrameImpl(MainFrameDefn):
         rootItem = self.m_tlFunctions.GetRootItem()
         for i in allOperations:
             funcItem = allOperations[i]
-            child = self.m_tlFunctions.AppendItem(rootItem, funcItem['Name'], -1, -1, i)
+            child = self.m_tlFunctions.AppendItem(rootItem, funcItem['Name'], -1, -1, funcItem)
 
     
     
@@ -37,20 +39,13 @@ class MainFrameImpl(MainFrameDefn):
         else:
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         h, w = image.shape[:2]
-        # The following conversion fails on Raspberry Pi.
         bitmap = wx.BitmapFromBuffer(w, h, image)
         return bitmap
     
     def loadBitmap(self, filePath):
         try:
-            height, width = self.m_pnlImage.Size
-#             self.baseImage = cv2.cvtColor(cv2.imread(filePath), cv2.COLOR_BGR2RGB)
             self.baseImage = cv2.imread(filePath)
-            self.m_pnlImage.Refresh()
-#             self.bmp = wx.BitmapFromBuffer(width, height, self.baseImage)
-#             dc = wx.ClientDC(self.m_pnlImage)
-#             dc = wx.BufferedDC(self.m_pnlImage)
-#             dc.DrawBitmap(self.bmp, 0, 0)
+            self.m_pnlImageOrg.Refresh()
         except IOError:
             print("Cannot open file '%s'." % filePath)
 
@@ -59,17 +54,55 @@ class MainFrameImpl(MainFrameDefn):
 # Event handlers
 #==============================================================================================================
     
-    def OnPanelPaint( self, event ):
-        print("Painting")
-        height, width = self.m_pnlImage.Size
+    def OnPanelPaintOrg( self, event ):
+        if self.baseImage is None:
+            return
+        height, width = self.m_pnlImageOrg.Size
         img = cv2.resize(self.baseImage, (height, width), cv2.INTER_LINEAR)
         self.bmp = self.wxBitmapFromCvImage(img)
-        dc = wx.PaintDC(self.m_pnlImage)
+        dc = wx.PaintDC(self.m_pnlImageOrg)
 #         dc = wx.ClientDC(self.m_pnlImage)
         dc.DrawBitmap(self.bmp, 0, 0)
     
+    def OnPanelPaintRes( self, event ):
+        if self.baseImage is None:
+            return
+        height, width = self.m_pnlImageRes.Size
+        img = cv2.resize(self.baseImage, (height, width), cv2.INTER_LINEAR)
+        
+        if self.blur and self.m_BlurKernel.Value >= 2:
+            val = self.m_BlurKernel.Value
+            img = cv2.medianBlur(img, val)
+        
+        if self.canny:
+            valTh1 = self.m_CannyTh1.Value
+            valTh2 = self.m_CannyTh2.Value
+            img = cv2.Canny(img, valTh1, valTh2)
+            im2, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            lstItems = [str(x).replace("\n","") for x in contours]
+            self.m_lstEdges.InsertItems(lstItems, self.m_lstEdges.GetCount())
+        
+        if not self.plotContour is None:
+            plotContour = eval(self.plotContour)
+            img = cv2.drawContours(img, plotContour, -1, (0,255,0))
+             
+            
+        self.bmp = self.wxBitmapFromCvImage(img)
+        dc = wx.PaintDC(self.m_pnlImageRes)
+#         dc = wx.ClientDC(self.m_pnlImage)
+        dc.DrawBitmap(self.bmp, 0, 0)
+        
+        
+        
+    def OnListBoxContoursSelect( self, event ):
+        nitem = self.m_lstEdges.GetSelection()
+        cnt = self.m_lstEdges.GetString(nitem)
+        self.plotContour = cnt
+        self.m_pnlImageRes.Refresh()
+        
+    
     def OnMenuFileOpenSelect( self, event ):
-        fileWildcards = "JPEG files (.jpg)|.jpg|TIFF files (.tif)|.tif|PNG files (.png)|.png"
+        fileWildcards = "All files (*.*)|*.*|JPEG files (*.jpg)|*.jpg|TIFF files (*.tif)|*.tif|PNG files (*.png)|*.png"
         with wx.FileDialog(self, "Open Image file", wildcard=fileWildcards,style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
             
             if fileDialog.ShowModal() == wx.ID_CANCEL:
@@ -79,3 +112,28 @@ class MainFrameImpl(MainFrameDefn):
             fileDialog.Hide()
             self.loadBitmap(pathname)
             
+    def OnCbBlurChange( self, event ):
+        if self.m_cbBlur.Value:
+            self.blur = True
+        else:
+            self.blur = False
+        self.m_pnlImageRes.Refresh()
+        
+    def OnScrollBlurKernelChanged( self, event ):
+        valKernel = self.m_BlurKernel.Value
+        self.m_txtBlurKernel.SetLabel("Kernel {0}".format(valKernel))
+        self.m_pnlImageRes.Refresh()
+            
+    def OnCbCannyChange( self, event ):
+        if self.m_cbCanny.Value:
+            self.canny = True
+        else:
+            self.canny = False
+        self.m_pnlImageRes.Refresh()
+        
+    def OnScrollCannyChanged( self, event ):
+        valTh1 = self.m_CannyTh1.Value
+        valTh2 = self.m_CannyTh2.Value
+        self.m_txtTh1Value.SetLabel("Threshold 1 {0}".format(valTh1))
+        self.m_txtTh2Value.SetLabel("Threshold 2 {0}".format(valTh2))
+        self.m_pnlImageRes.Refresh()
--- GUILayouts/OpenCVExplorerGUI.py
+++ GUILayouts/OpenCVExplorerGUI.py
@@ -18,7 +18,7 @@ import wx.dataview
 class MainFrameDefn ( wx.Frame ):
 	
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"OpenCV Explorer", pos = wx.DefaultPosition, size = wx.Size( 781,574 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"OpenCV Explorer", pos = wx.DefaultPosition, size = wx.Size( 781,693 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 		
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 		
@@ -26,17 +26,102 @@ class MainFrameDefn ( wx.Frame ):
 		
 		bszMainContent = wx.BoxSizer( wx.HORIZONTAL )
 		
+		bSizer4 = wx.BoxSizer( wx.VERTICAL )
+		
 		self.m_tlFunctions = wx.dataview.TreeListCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.dataview.TL_DEFAULT_STYLE )
-		self.m_tlFunctions.AppendColumn( u"Functions", wx.COL_WIDTH_DEFAULT, wx.ALIGN_LEFT, wx.COL_RESIZABLE )
 		
-		bszMainContent.Add( self.m_tlFunctions, 25, wx.EXPAND |wx.ALL, 5 )
+		bSizer4.Add( self.m_tlFunctions, 25, wx.EXPAND |wx.ALL, 5 )
 		
-		self.m_pnlImage = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		bszMainContent.Add( self.m_pnlImage, 70, wx.EXPAND |wx.ALL, 5 )
+		self.m_pnlTools = wx.ScrolledWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.HSCROLL|wx.VSCROLL )
+		self.m_pnlTools.SetScrollRate( 5, 5 )
+		bSizer5 = wx.BoxSizer( wx.VERTICAL )
 		
-		m_lbALayersChoices = []
-		self.m_lbALayers = wx.ListBox( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_lbALayersChoices, 0 )
-		bszMainContent.Add( self.m_lbALayers, 25, wx.ALL|wx.EXPAND, 5 )
+		self.m_panel4 = wx.Panel( self.m_pnlTools, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.BORDER_SIMPLE|wx.TAB_TRAVERSAL )
+		bSizer6 = wx.BoxSizer( wx.VERTICAL )
+		
+		self.m_cbBlur = wx.CheckBox( self.m_panel4, wx.ID_ANY, u"Blur", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer6.Add( self.m_cbBlur, 0, wx.ALL, 5 )
+		
+		self.m_BlurKernel = wx.Slider( self.m_panel4, wx.ID_ANY, 0, 0, 20, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		bSizer6.Add( self.m_BlurKernel, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		self.m_txtBlurKernel = wx.StaticText( self.m_panel4, wx.ID_ANY, u"Kernel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_txtBlurKernel.Wrap( -1 )
+		
+		bSizer6.Add( self.m_txtBlurKernel, 0, wx.ALL, 5 )
+		
+		
+		self.m_panel4.SetSizer( bSizer6 )
+		self.m_panel4.Layout()
+		bSizer6.Fit( self.m_panel4 )
+		bSizer5.Add( self.m_panel4, 7, wx.EXPAND |wx.ALL, 5 )
+		
+		self.m_panel5 = wx.Panel( self.m_pnlTools, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.BORDER_DEFAULT|wx.BORDER_SIMPLE|wx.TAB_TRAVERSAL )
+		bSizer7 = wx.BoxSizer( wx.VERTICAL )
+		
+		self.m_cbCanny = wx.CheckBox( self.m_panel5, wx.ID_ANY, u"Canny Edge Detection", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer7.Add( self.m_cbCanny, 0, wx.ALL, 5 )
+		
+		self.m_CannyTh1 = wx.Slider( self.m_panel5, wx.ID_ANY, 0, 0, 500, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		bSizer7.Add( self.m_CannyTh1, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		self.m_txtTh1Value = wx.StaticText( self.m_panel5, wx.ID_ANY, u"Threshold 1", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_txtTh1Value.Wrap( -1 )
+		
+		bSizer7.Add( self.m_txtTh1Value, 0, wx.ALL, 5 )
+		
+		self.m_CannyTh2 = wx.Slider( self.m_panel5, wx.ID_ANY, 0, 0, 500, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		bSizer7.Add( self.m_CannyTh2, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		self.m_txtTh2Value = wx.StaticText( self.m_panel5, wx.ID_ANY, u"Threshold 2", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_txtTh2Value.Wrap( -1 )
+		
+		bSizer7.Add( self.m_txtTh2Value, 0, wx.ALL, 5 )
+		
+		
+		self.m_panel5.SetSizer( bSizer7 )
+		self.m_panel5.Layout()
+		bSizer7.Fit( self.m_panel5 )
+		bSizer5.Add( self.m_panel5, 10, wx.EXPAND |wx.ALL, 5 )
+		
+		self.m_panel6 = wx.Panel( self.m_pnlTools, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer8 = wx.BoxSizer( wx.VERTICAL )
+		
+		m_lstEdgesChoices = []
+		self.m_lstEdges = wx.ListBox( self.m_panel6, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_lstEdgesChoices, 0 )
+		bSizer8.Add( self.m_lstEdges, 1, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		self.m_panel6.SetSizer( bSizer8 )
+		self.m_panel6.Layout()
+		bSizer8.Fit( self.m_panel6 )
+		bSizer5.Add( self.m_panel6, 10, wx.EXPAND |wx.ALL, 5 )
+		
+		self.m_panel7 = wx.Panel( self.m_pnlTools, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer5.Add( self.m_panel7, 10, wx.EXPAND |wx.ALL, 5 )
+		
+		self.m_panel8 = wx.Panel( self.m_pnlTools, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer5.Add( self.m_panel8, 10, wx.EXPAND |wx.ALL, 5 )
+		
+		
+		self.m_pnlTools.SetSizer( bSizer5 )
+		self.m_pnlTools.Layout()
+		bSizer5.Fit( self.m_pnlTools )
+		bSizer4.Add( self.m_pnlTools, 66, wx.EXPAND |wx.ALL, 5 )
+		
+		
+		bszMainContent.Add( bSizer4, 30, wx.EXPAND, 5 )
+		
+		bszPanels = wx.BoxSizer( wx.VERTICAL )
+		
+		self.m_pnlImageOrg = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.FULL_REPAINT_ON_RESIZE|wx.TAB_TRAVERSAL )
+		bszPanels.Add( self.m_pnlImageOrg, 50, wx.EXPAND |wx.ALL, 5 )
+		
+		self.m_pnlImageRes = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bszPanels.Add( self.m_pnlImageRes, 50, wx.EXPAND |wx.ALL, 5 )
+		
+		
+		bszMainContent.Add( bszPanels, 70, wx.EXPAND, 5 )
 		
 		
 		bszMain.Add( bszMainContent, 15, wx.EXPAND, 5 )
@@ -57,7 +142,15 @@ class MainFrameDefn ( wx.Frame ):
 		self.Centre( wx.BOTH )
 		
 		# Connect Events
-		self.m_pnlImage.Bind( wx.EVT_PAINT, self.OnPanelPaint )
+		self.m_tlFunctions.Bind( wx.dataview.EVT_TREELIST_SELECTION_CHANGED, self.OnTreelistSelectionChanged )
+		self.m_cbBlur.Bind( wx.EVT_CHECKBOX, self.OnCbBlurChange )
+		self.m_BlurKernel.Bind( wx.EVT_SCROLL_CHANGED, self.OnScrollBlurKernelChanged )
+		self.m_cbCanny.Bind( wx.EVT_CHECKBOX, self.OnCbCannyChange )
+		self.m_CannyTh1.Bind( wx.EVT_SCROLL_CHANGED, self.OnScrollCannyChanged )
+		self.m_CannyTh2.Bind( wx.EVT_SCROLL_CHANGED, self.OnScrollCannyChanged )
+		self.m_lstEdges.Bind( wx.EVT_LISTBOX, self.OnListBoxContoursSelect )
+		self.m_pnlImageOrg.Bind( wx.EVT_PAINT, self.OnPanelPaintOrg )
+		self.m_pnlImageRes.Bind( wx.EVT_PAINT, self.OnPanelPaintRes )
 		self.Bind( wx.EVT_MENU, self.OnMenuFileOpenSelect, id = self.m_mnuItemFileOpen.GetId() )
 	
 	def __del__( self ):
@@ -65,28 +158,32 @@ class MainFrameDefn ( wx.Frame ):
 	
 	
 	# Virtual event handlers, overide them in your derived class
-	def OnPanelPaint( self, event ):
+	def OnTreelistSelectionChanged( self, event ):
+		event.Skip()
+	
+	def OnCbBlurChange( self, event ):
+		event.Skip()
+	
+	def OnScrollBlurKernelChanged( self, event ):
+		event.Skip()
+	
+	def OnCbCannyChange( self, event ):
+		event.Skip()
+	
+	def OnScrollCannyChanged( self, event ):
+		event.Skip()
+	
+	
+	def OnListBoxContoursSelect( self, event ):
+		event.Skip()
+	
+	def OnPanelPaintOrg( self, event ):
+		event.Skip()
+	
+	def OnPanelPaintRes( self, event ):
 		event.Skip()
 	
 	def OnMenuFileOpenSelect( self, event ):
 		event.Skip()
-	
-
-###########################################################################
-## Class AlgoFrameDefn
-###########################################################################
-
-class AlgoFrameDefn ( wx.Frame ):
-	
-	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 608,400 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-		
-		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
-		
-		
-		self.Centre( wx.BOTH )
-	
-	def __del__( self ):
-		pass
 	
 
